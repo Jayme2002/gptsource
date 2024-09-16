6189c3df54a5d90017666a47f7fd96e4fb4eed90
Jayme Carey <jaymecarey@Jaymes-MacBook-Pro.local>
2024-09-16 15:03:53 -0700

    fixes for deployment

--- lib/db.ts
+++ lib/db.ts
@@ -1,23 +1,13 @@
 import { PrismaClient } from '@prisma/client'
-import { PrismaPlanetScale } from '@prisma/adapter-planetscale'
-import { Client } from '@planetscale/database'
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
 
-const connectionString = process.env.DATABASE_URL
-
-if (!connectionString) {
-  throw new Error('DATABASE_URL is not set')
-}
-
-const client = new Client({ url: connectionString })
-
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
-    adapter: new PrismaPlanetScale(client),
+    log: ['query'],
   })
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
