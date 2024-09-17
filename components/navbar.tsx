3e8cd2c7262b21e996e2a2719084b0fc58aa03cf
Jayme Carey <jaymecarey@Jaymes-MacBook-Pro.local>
2024-09-17 11:39:00 -0700

    Improvement of mobile functionality

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -24,14 +24,27 @@ const Navbar = ({ apiLimitCount, chats, isPro = false }: navbarProps) => {
     const pathname = usePathname();
     return (
         <div className="fixed top-0 left-0 md:left-80 right-0 z-50 border-b md:border-none md:min-h-[4rem] flex justify-between items-center px-2.5 py-1 bg-slate-900">
-            <div className="md:hidden flex items-center">
+            <div className="md:hidden flex items-center justify-between w-full">
                 <MobileSidebar apiLimitCount={apiLimitCount} chats={chats} isPro={isPro} />
-                <div className="ml-4">
-                    <FreeCounter apiLimitCount={apiLimitCount} isPro={isPro} variant="small" />
+                <FreeCounter apiLimitCount={apiLimitCount} isPro={isPro} />
+                <div className="flex items-center space-x-2">
+                    <UserButton
+                        appearance={{
+                            elements: {
+                                avatarBox: {
+                                    height: "36px",
+                                    width: "36px",
+                                },
+                            },
+                        }}
+                        afterSignOutUrl="/"
+                    />
+                    <Link href={"/settings"} className="cursor-pointer text-indigo-300">
+                        <Settings strokeWidth={1} className="h-[1.4rem] w-[1.4rem]" />
+                    </Link>
                 </div>
             </div>
-            <div className="hidden md:flex flex-1"></div>
-            <div className="flex items-center space-x-2">
+            <div className="hidden md:flex flex-1 justify-end items-center space-x-2">
                 <UserButton
                     appearance={{
                         elements: {
