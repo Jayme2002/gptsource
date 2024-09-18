5c8e808f57872e5ad29ca1f3ce915f0783b44528
Jayme Carey <jaymecarey@Jaymes-MacBook-Pro.local>
2024-09-18 09:17:12 -0700

    switched to gpt4o

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -73,7 +73,7 @@ export async function POST(req: Request) {
         }
 
         const response = await openai.createChatCompletion({
-            model: "gpt-3.5-turbo",
+            model: "gpt-4o",
             messages,
             stream: true,
         });
